811e4738175026c366f95c9f183a898dd504e34a
Shivansh Kumar Mishra <105987249+mishrashi@users.noreply.github.com>
2025-09-05 12:21:00 +0530

    Commit

---
+++ RLTSM1/src/main/java/com/RLTSM1/Rltsm1Application.java
@@ -0,0 +1,13 @@
+package com.RLTSM1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rltsm1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rltsm1Application.class, args);
+	}
+
+}
---
+++ RLTSM1/src/test/java/com/RLTSM1/Rltsm1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RLTSM1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rltsm1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
